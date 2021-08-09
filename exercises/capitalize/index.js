7c713deb4e01abb54eb718df5dd0649e1bbe2608
Adam Schwartz <adamberard24@gmail.com>
2021-08-09 13:59:06 -0400

    Added the things

---
+++ exercises/capitalize/index.js
@@ -0,0 +1,50 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+
+  let result = str[0].toUpperCase()
+
+  for (let i = 1; i < str.length; i++){
+      if (str[i - 1] === ' ') {
+          result += str[i].toUpperCase()
+      } else {
+          result += str[i]
+      }
+  }
+return result
+}
+
+module.exports = capitalize;
+
+// MY SOLUTION
+// function capitalize(str) {
+
+//     let strArr = str.split(" ")
+//     let capitalized = []
+
+//     for (let word of strArr) {
+//        capitalized.push(word[0].toUpperCase().concat(word.slice(1)))
+        
+
+//     }
+
+//     return capitalized.join(' ')
+// }
+
+// SOLUTION 1
+// function capitalize(str) {
+
+//     const words = []
+  
+//     for (let word of str.split(' ')) {
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+//     return words.join(' ')
+//   }
